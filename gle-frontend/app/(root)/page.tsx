0198025e2b42c9c3fb8de8812fbe6437d82a066d
Shane Walsh <swalsh818@gmail.com>
2024-10-21 21:31:47 +0100

    Implemented ClerkJS authentication

--- gle-frontend/app/(root)/page.tsx
+++ gle-frontend/app/(root)/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl">Home</h1>
-      
-    </div>
+    <main>
+      <h1 className="text-4xl">Exper</h1>
+    </main>
   );
 }
