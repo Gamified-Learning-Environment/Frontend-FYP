87cc977239526706dd31fea21545c93ed4003482
Shane Walsh <swalsh818@gmail.com>
2024-10-30 13:42:32 +0000

    Developed Footer

--- gle-frontend/components/shared/Footer.tsx
+++ gle-frontend/components/shared/Footer.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const Footer = () => {
+const footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image 
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>
+          Check out Repo at <a href="https://github.com/ShanewalshGit" target="_blank" rel="noopener noreferrer">ShanewalshGit</a>
+        </p>      
+      </div>
+    </footer>
   )
 }
 
-export default Footer
+export default footer
